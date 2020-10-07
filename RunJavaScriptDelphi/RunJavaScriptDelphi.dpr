c138a7036bdb5963ef1256891e9b9caa350e5ede
sergey <shirokov@clevercomponents.com>
2020-10-07 23:20:52 +0300

    Project dependencies fixes.

--- RunJavaScriptDelphi/RunJavaScriptDelphi.dpr
+++ RunJavaScriptDelphi/RunJavaScriptDelphi.dpr
@@ -1,23 +1,23 @@
 program RunJavaScriptDelphi;
 
 uses
-  FastMM4 in '..\..\FastMM\FastMM4.pas',
-  FastMM4Messages in '..\..\FastMM\FastMM4Messages.pas',
+  FastMM4,
+  FastMM4Messages,
   Vcl.Forms,
   Unit1 in 'Unit1.pas' {Form1},
-  NSPRAPI in '..\..\23\mORMot\SyNode\NSPRAPI.pas',
-  SpiderMonkey in '..\..\23\mORMot\SyNode\SpiderMonkey.pas',
-  SyNode in '..\..\23\mORMot\SyNode\SyNode.pas',
-  SyNodeBinding_buffer in '..\..\23\mORMot\SyNode\SyNodeBinding_buffer.pas',
-  SyNodeBinding_const in '..\..\23\mORMot\SyNode\SyNodeBinding_const.pas',
-  SyNodeBinding_fs in '..\..\23\mORMot\SyNode\SyNodeBinding_fs.pas',
-  SyNodeBinding_HTTPClient in '..\..\23\mORMot\SyNode\SyNodeBinding_HTTPClient.pas',
-  SyNodeBinding_util in '..\..\23\mORMot\SyNode\SyNodeBinding_util.pas',
-  SyNodeBinding_uv in '..\..\23\mORMot\SyNode\SyNodeBinding_uv.pas',
-  SyNodeBinding_worker in '..\..\23\mORMot\SyNode\SyNodeBinding_worker.pas',
-  SyNodeReadWrite in '..\..\23\mORMot\SyNode\SyNodeReadWrite.pas',
-  SyNodeSimpleProto in '..\..\23\mORMot\SyNode\SyNodeSimpleProto.pas',
-  SyNodeProto in '..\..\23\mORMot\SyNode\SyNodeProto.pas';
+  NSPRAPI,
+  SpiderMonkey,
+  SyNode,
+  SyNodeBinding_buffer,
+  SyNodeBinding_const,
+  SyNodeBinding_fs,
+  SyNodeBinding_HTTPClient,
+  SyNodeBinding_util,
+  SyNodeBinding_uv,
+  SyNodeBinding_worker,
+  SyNodeReadWrite,
+  SyNodeSimpleProto,
+  SyNodeProto;
 
 {$R *.res}
 
